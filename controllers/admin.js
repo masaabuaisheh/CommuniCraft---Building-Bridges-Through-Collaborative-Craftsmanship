bb50e57431607cf90c7aca20b7343bd2e48cc73e
nancysawalmeh <nsawa957@gmail.com>
2024-03-21 13:41:47 +0200

    update1

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const db = require('../config/dbconnection');
 const randomstring = require('randomstring');
 
+
 const showallprojects = (req, res) => {
    
     if(req.user.role != "Admin"){
